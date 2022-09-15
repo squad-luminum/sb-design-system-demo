37ff39a8f3716cbd1c3d0cc567917e1d8518acd6
Chaz Gatian <chaz.gatian@tanium.com>
2022-09-15 19:05:30 -0400

    improved transformation script

--- tokens/transformTokens.ts
+++ tokens/transformTokens.ts
@@ -1,60 +1,35 @@
 import StyleDictionary from 'style-dictionary';
 import { paramCase } from 'change-case';
 
-const baseConfig = require('./style-dictionary-config.json')
-// StyleDictionary.registerTransform({
-//   name: 'size/px',
-//   type: 'value',
-//   matcher: token => {
-//     return (token.unit === 'pixel' || token.type === 'dimension') && token.value !== 0
-//   },
-//   transformer: token => {
-//     return `${token.value}px`
-//   }
-// })
-
-// StyleDictionary.registerTransform({
-//   name: 'size/percent',
-//   type: 'value',
-//   matcher: token => {
-//     return token.unit === 'percent' && token.value !== 0
-//   },
-//   transformer: token => {
-//     return `${token.value}%`
-//   }
-// })
-
-// StyleDictionary.registerTransformGroup({
-//   name: 'custom/css',
-//   transforms: StyleDictionary.transformGroup['css'].concat([
-//     'size/px',
-//     'size/percent'
-//   ])
-// })
-
-
-// StyleDictionary.registerFilter({
-//   name: 'validToken',
-//   matcher: function(token) {
-//     return ['dimension', 'string', 'number', 'color'].includes(token.type)
-//   }
-// })
-
-// 'name/cti/kebab': {
-//     type: 'name',
-
-//   },
+const themes = [`luminumLight`, `luminumDark`, `blueStarLight`];
 
 StyleDictionary.registerTransform({
     name: 'betterKebab',
     type: 'name',
          transformer: function(token, options) {
-       return paramCase([options!.prefix].concat(token.path).join(' '));
+       return paramCase([options!.prefix].concat(token.path).join(' '), { 
+        splitRegexp: /([a-z])([A-Z])/g,
+       });
      }
 })
 
-
-
-const StyleDictionaryExtended = StyleDictionary.extend(baseConfig)
-
-StyleDictionaryExtended.buildAllPlatforms()
+themes.forEach(theme => {
+  StyleDictionary.extend({
+    source: [`dist/${theme}.tokens.json`],
+    platforms: {
+      "web": {
+        transformGroup: "web",
+        transforms: ["betterKebab"],
+        buildPath: `dist/css/`,
+        files: [{
+          "destination": `${theme}.css`,
+          "format": "css/variables"
+        }],
+        options : {
+          "showFileHeader": false,
+          "outputReferences": true
+        }
+      }
+    }
+  }).buildPlatform('web');
+});
